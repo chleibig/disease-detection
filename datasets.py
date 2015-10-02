353bb6e8d74871fac8d1ee4e8dbfd626a8536973
Christian Leibig <christian.leibig@uni-tuebingen.de>
2015-10-02 14:03:40 +0200

    Refactor datasets
    no separate folder for datasets anymore. Some Abstract Dataset funcionality, concrete implementation
    for Kaggle's Diabetic Retinopathy is ready

---
+++ datasets.py
@@ -0,0 +1,147 @@
+from abc import ABCMeta, abstractmethod
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+import skimage.transform
+from lasagne.utils import floatX
+
+
+class Dataset(object):
+    """Base class for actual datasets"""
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def load_batch(self, indices):
+        """
+        Load a batch of data samples together with labels
+
+        Parameters
+        ----------
+        indices: array-like, shape = (n_samples,)
+            with respect to the entire data set
+
+        Returns
+        -------
+        X : numpy array, shape = (n_samples, n_channels, n_rows, n_columns)
+            batch of data
+        y : numpy array, shape = (n_samples,)
+
+        """
+
+    def iterate_minibatches(self, indices, batchsize, shuffle=False):
+        """
+        Generator that yields a batch of data together with labels
+
+        Parameters
+        ----------
+        indices : array-like, shape = (n_samples,)
+            with respect to the entire data set
+        batchsize : int
+        shuffle : boolean (False by default)
+            shuffle indices
+
+        Returns
+        -------
+        X : numpy array, shape = (n_samples, n_channels, n_rows, n_columns)
+            batch of data
+        y : numpy array, shape = (n_samples,)
+
+        Notes
+        -----
+        Currently len(indices)%batchsize samples are not used!
+
+        """
+        if shuffle:
+            np.random.shuffle(indices)
+        for start_idx in range(0, len(indices) - batchsize + 1, batchsize):
+            excerpt = indices[start_idx:start_idx + batchsize]
+            yield self.load_batch(excerpt)
+
+
+class KaggleDR(Dataset):
+    """
+    Provides access to data from Kaggle's Diabetic Retinopathy competition.
+
+    """
+
+    def __init__(self, path_data=None, filename_targets=None):
+        self.path_data = path_data
+        self.filename_targets = filename_targets
+
+    def load_image(self, filename):
+        """
+        Load image.
+
+        Parameters
+        ----------
+        filename : string
+            relative filename (path to image folder gets prefixed)
+
+        Returns
+        -------
+        image : numpy array, shape = (n_rows, n_columns, n_channels)
+
+        """
+
+        filename = os.path.join(self.path_data, filename + '.jpeg')
+        return np.array(Image.open(filename))
+
+    @staticmethod
+    def prep_image(im):
+        """
+        Preprocess image.
+
+        Resizes smaller spatial extent to 256 pixels while preserving the
+        aspect ratio. Central part of image is cropped to 224 x 224.
+        No mean subtraction. Colour channels are inverted: RGB -> BGR
+        Dimensions get reordered.
+
+        Parameters
+        ----------
+        im : numpy array, shape = (n_rows, n_columns, n_channels)
+
+        Returns
+        -------
+        processed image : numpy array, shape = (n_channels, n_rows, n_columns)
+                                       dtype = floatX
+
+        """
+
+        # Resize so smallest dim = 256, preserving aspect ratio
+        h, w, _ = im.shape
+        if h < w:
+            im = skimage.transform.resize(im, (256, w*256/h),
+                                          preserve_range=True)
+        else:
+            im = skimage.transform.resize(im, (h*256/w, 256),
+                                          preserve_range=True)
+        # Central crop to 224x224
+        h, w, _ = im.shape
+        im = im[h//2-112:h//2+112, w//2-112:w//2+112]
+        # Returned image should be (n_channels, n_rows, n_columns)
+        im = np.transpose(im, (2, 0, 1))
+        # Convert to BGR
+        im = im[::-1, :, :]
+        return floatX(im)
+
+    def load_batch(self, indices):
+        """
+        Load batch of preprocessed data samples together with labels
+
+        Parameters
+        ----------
+
+        indices : array_like, shape = (batchsize,)
+            absolute index values refer to position in trainLabels.csv
+
+        """
+        labels = pd.read_csv(self.filename_targets)
+        image_filenames = labels['image'][indices]
+        y = np.array(labels['level'][indices])
+        X = np.array([self.prep_image(self.load_image(fn)) for fn in
+                      image_filenames])
+        assert len(X) == len(y)
+        return X, y
+
