75582a279bbea4c6c0975968db6a8afcaa06b3b9
Christian Leibig <christian.leibig@uni-tuebingen.de>
2015-10-14 19:09:47 +0200

    extract_features.py : rewrite to batch processing
    Batch processing is necessary, because not all images fit
    into GPU memory: Even though the preprocessed images amount to 819MB,
    the corresponding numpy array holding all 35126 images
    amounts to ~ 21 GB as each float32 element consumes 28 bytes!

--- extract_features.py
+++ extract_features.py
@@ -8,64 +8,64 @@ Christian Leibig, 2015
 """
 from __future__ import division
 import time
-
 import click
 import numpy as np
-import glob
-
-
-def get_image_files(data_dir, left_only=False):
-    fs = glob.glob('{}/*.jpeg'.format(data_dir))
-    if left_only:
-        fs = [f for f in fs if 'left' in f]
-    return np.array(sorted(fs))
 
 
 @click.command()
 @click.option('--source_dir', default=None, show_default=True,
               help="Directory with images to be transformed.")
+@click.option('--filename_targets', default=None, show_default=True,
+              help="Absolute filename of trainLabels.csv")
+@click.option('--batch_size', default=256, show_default=True,
+              help="Number of samples to be passed through the network at "
+                   "once.")
 @click.option('--outfile', default='feature_activations.npy',
               show_default=True,
               help="Filename for saving the extracted features.")
 @click.option('--last_layer', default='fc7', show_default=True,
               help="Layer up to which features shall be computed.")
-def main(source_dir, outfile, last_layer):
+def main(source_dir, filename_targets, batch_size, outfile, last_layer):
     import theano
     import theano.tensor as T
     import lasagne
-    from PIL import Image
 
     from modelzoo import vgg19
     from datasets import KaggleDR
-    ###########################################################################
-    # Setup pretrained network: Here VGG19
-    ###########################################################################
+
     network = vgg19.build_model(load_weights=True)
-    ###########################################################################
-    # Extract features
     output_layer = network[last_layer]
-
-    print("Loading images from {}...".format(source_dir))
-    start_time = time.time()
-    file_names = get_image_files(source_dir)
-    X = np.array([KaggleDR.prepare_image(np.array(Image.open(fn)))
-                  for fn in file_names])
-    print('took {:6.1f} seconds'.format(time.time() - start_time))
-
-    # Prepare theano variables
     input_var = T.tensor4('inputs')
-    # plug symbolic input to network
     network['input'].input_var = input_var
 
     feature_activations = lasagne.layers.get_output(output_layer)
     forward_pass = theano.function([input_var], feature_activations)
 
-    print("Computing features of", len(X), "sample(s)...")
-    start_time = time.time()
-    outputs = forward_pass(X)
-    print("took", np.round((time.time() - start_time), 3), "sec.")
+    kdr = KaggleDR(path_data=source_dir, filename_targets=filename_targets)
 
+    start_time = time.time()
+    idx = np.arange(kdr.n_samples)
+    print("Loading images from {}...".format(source_dir))
+    kdr.load_data(idx)
+    print('took {:6.1f} seconds'.format(time.time() - start_time))
+
+    outputs = np.empty_like(kdr.X)
+    i = 0
+    n_batches = np.ceil(kdr.n_samples/batch_size)
+    print("Computing features of", kdr.n_samples, "sample(s)...")
+    start_time = time.time()
+    for batch in kdr.iterate_minibatches(idx, batch_size):
+            print("Working on batch {}/{}".format(i, n_batches))
+            inputs, _ = batch
+            outputs[i*batch_size:min((i+1)*batch_size, kdr.n_samples)] = \
+                forward_pass(inputs)
+            i += 1
+
+    print("Forward pass of", kdr.n_samples, "took",
+          np.round((time.time() - start_time), 3), "sec.")
+    print("Writing features to disk...")
     np.save(outfile, outputs)
+    print("Done.")
 
 
 if __name__ == '__main__':
