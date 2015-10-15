8ca580b5e4e9ded561fe47d151ed52134b59bbfc
Christian Leibig <christian.leibig@uni-tuebingen.de>
2015-10-15 16:16:09 +0200

    transfer_learning_kaggle_dr.py : Perform log reg features
    Major rewrite.
    Changes:
    - Use precomputed features instead of performing forward passes
    through pretrained network all over again
    - shorter variable names
    - kera's progbar instead of handwritten print() calls

--- transfer_learning_kaggle_dr.py
+++ transfer_learning_kaggle_dr.py
@@ -2,12 +2,6 @@ from __future__ import print_function
 
 import click
 
-# TODO: - resolve memory issue -> check theano docu on python memory management
-# TODO: - set trainable tag of convnet params to false
-# --> in build_model() set tags trainable=False!
-# TODO: - use both left and right eye data for each patient
-
-
 @click.command()
 @click.option('--path', default=None, show_default=True,
               help="Path to trainLabels.csv and extracted_features.npy.")
@@ -24,53 +18,44 @@ def main(path, batch_size, n_epoch, split, model_file):
     """
 
     import os
-    import time
     import numpy as np
     import theano
     import theano.tensor as T
     import lasagne
-    from lasagne.layers import DenseLayer, NonlinearityLayer
-    from lasagne.nonlinearities import softmax
+    from lasagne.layers import InputLayer, DenseLayer, NonlinearityLayer
+    from lasagne.utils import floatX
+    from keras.utils.generic_utils import Progbar
 
-    from modelzoo import vgg19
     from datasets import KaggleDR
 
-    ###########################################################################
-    # Parameters
-    ###########################################################################
-    train_frac, val_frac, test_frac = split
-
-    # Prepare theano variables for inputs and targets
-    input_var = T.tensor4('inputs')
-    target_var = T.ivector('targets')
+    X = T.matrix('X')
+    y = T.ivector('y')
 
     ###########################################################################
-    # Setup pretrained network: Here VGG19
+    # Load features obtained via forward pass through pretrained network
     ###########################################################################
-    network = vgg19.build_model(load_weights=True)
-    ###########################################################################
-    # Transfer Learning: Adjust output layer to new task
-    ###########################################################################
-    network['fc8'] = DenseLayer(network['fc7'], num_units=5, nonlinearity=None)
-    network['prob'] = NonlinearityLayer(network['fc8'], softmax)
-    output_layer = network['prob']
-
-    # plug symbolic input to network
-    network['input'].input_var = input_var
-
-    # later on this will be filled with values from the actual dataset:
     kdr = KaggleDR(path_data=os.path.join(path, 'train'),
                    filename_targets=os.path.join(path, 'trainLabels.csv'))
 
-    # Scalar loss expression to be minimized during training:
-    prediction = lasagne.layers.get_output(output_layer)
-    loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-    loss = loss.mean()
+    kdr.X = floatX(np.load(os.path.join(path, 'extracted_features.npy')))
+    n_samples, n_features = kdr.X.shape
+    # assert that we have features for all labels stored in kdr.y
+    assert n_samples == kdr.n_samples
+    kdr.indices_in_X = np.arange(n_samples)
 
     ###########################################################################
-    # Retrain only parameters of the last layer
+    # Transfer Learning: Train logistic regression on extracted features
     ###########################################################################
-    params = network['fc8'].get_params()
+    l_in = InputLayer((batch_size, n_features), input_var=X)
+    l_hidden = DenseLayer(l_in, num_units=5, nonlinearity=None)
+    l_out = NonlinearityLayer(l_hidden, lasagne.nonlinearities.softmax)
+
+    # Scalar loss expression to be minimized during training:
+    y_pred = lasagne.layers.get_output(l_out)
+    loss = lasagne.objectives.categorical_crossentropy(y_pred, y)
+    loss = loss.mean()
+
+    params = lasagne.layers.get_all_params(l_out, trainable=True)
 
     updates = lasagne.updates.nesterov_momentum(loss, params,
                                                 learning_rate=0.01,
@@ -78,79 +63,57 @@ def main(path, batch_size, n_epoch, split, model_file):
 
     # Scalar loss expression for testing - only necessary if stochasticity such
     # as dropout is involved during training
-    test_prediction = lasagne.layers.get_output(output_layer,
-                                                deterministic=True)
-    test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
-                                                            target_var)
+    test_y_pred = lasagne.layers.get_output(l_out, deterministic=True)
+    test_loss = lasagne.objectives.categorical_crossentropy(test_y_pred, y)
     test_loss = test_loss.mean()
     # Theano expression for classification accuracy
-    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+    test_acc = T.mean(T.eq(T.argmax(test_y_pred, axis=1), y),
                       dtype=theano.config.floatX)
 
     # Function for one training step on minibatch
-    train_fn = theano.function([input_var, target_var], loss, updates=updates)
+    train_fn = theano.function([X, y], loss, updates=updates)
 
     # Function to compute val loss and accuracy
-    val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+    val_fn = theano.function([X, y], [test_loss, test_acc])
 
-    train_indices, val_indices, test_indices = kdr.generate_indices(train_frac,
-                                                                    val_frac,
-                                                                    test_frac)
+    idx_train, idx_val, idx_test = kdr.generate_indices(*split)
 
     ###########################################################################
     # Training
     ###########################################################################
-    print("Starting training...")
     for epoch in range(n_epoch):
-        # In each epoch, we do a full pass over the training data:
-        train_err = 0
-        train_batches = 0
-        start_time = time.time()
-        for batch in kdr.iterate_minibatches(train_indices, batch_size):
-            inputs, targets = batch
-            train_err += train_fn(inputs, targets)
-            train_batches += 1
+        print('-'*40)
+        print('Epoch', epoch)
+        print('-'*40)
+        print("Training...")
 
-        # And a full pass over the validation data:
-        val_err = 0
-        val_acc = 0
-        val_batches = 0
-        for batch in kdr.iterate_minibatches(val_indices, batch_size,
+        progbar = Progbar(len(idx_train))
+        for batch in kdr.iterate_minibatches(idx_train, batch_size):
+            inputs, targets = batch
+            err = train_fn(inputs, targets)
+            progbar.add(inputs.shape[0], values=[("train loss", err)])
+
+        progbar = Progbar(len(idx_val))
+        for batch in kdr.iterate_minibatches(idx_val, batch_size,
                                              shuffle=False):
             inputs, targets = batch
             err, acc = val_fn(inputs, targets)
-            val_err += err
-            val_acc += acc
-            val_batches += 1
-
-        # Then we print the results for this epoch:
-        print("Epoch {} of {} took {:.3f}s".format(
-            epoch + 1, n_epoch, time.time() - start_time))
-        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
-        print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
-        print("  validation accuracy:\t\t{:.2f} %".format(
-            val_acc / val_batches * 100))
+            progbar.add(inputs.shape[0], values=[("validation loss", err),
+                                                 ("validation accuracy", acc)])
 
     ###########################################################################
     # Testing
     ###########################################################################
-    test_err = 0
-    test_acc = 0
-    test_batches = 0
-    for batch in kdr.iterate_minibatches(test_indices, batch_size,
+    progbar = Progbar(len(idx_test))
+    for batch in kdr.iterate_minibatches(idx_test, batch_size,
                                          shuffle=False):
         inputs, targets = batch
         err, acc = val_fn(inputs, targets)
-        test_err += err
-        test_acc += acc
-        test_batches += 1
-    print("Final results:")
-    print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
-    print("  test accuracy:\t\t{:.2f} %".format(
-        test_acc / test_batches * 100))
+        progbar.add(inputs.shape[0], values=[("test loss", err),
+                                             ("test accuracy", acc)])
 
     np.savez(os.path.join(path, model_file),
-             *lasagne.layers.get_all_param_values(network))
+             *lasagne.layers.get_all_param_values(l_out))
 
 if __name__ == '__main__':
     main()
