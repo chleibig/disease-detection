114776dc5c0a49dfe9d7001b51e7dce8fe7051b8
Christian Leibig <christian.leibig@uni-tuebingen.de>
2016-01-04 18:31:08 +0100

    Parametrize test_convert for o_O and JF preprocessing

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,4 +1,10 @@
-def test_convert_o_O(tmpdir):
+import pytest
+
+
+@pytest.mark.parametrize("converter, ref_dir",
+                         [('o_O', 'tests/ref_data/KDR/sample_o_O_512'),
+                          ('JF', 'tests/ref_data/KDR/sample_JF_512')])
+def test_convert(converter, ref_dir, tmpdir):
     import os
     from PIL import Image
     import numpy.testing as nt
@@ -9,16 +15,18 @@ def test_convert_o_O(tmpdir):
     extension = 'jpeg'
     n_proc = 4
 
-    os.system(" ".join(["python convert_o_O.py",
+    converter_fun = 'convert_' + converter + '.py'
+
+    os.system(" ".join(["python", converter_fun,
                         "--directory", directory,
                         "--convert_directory", convert_directory,
                         "--crop_size", str(crop_size),
                         "--extension", extension,
                         "--n_proc", str(n_proc)]))
 
-    ref_dir = 'tests/ref_data/KDR/sample_o_O_512'
-
     for filename in os.listdir(ref_dir):
         ref_image = Image.open(os.path.join(ref_dir, filename))
         convert_image = Image.open(os.path.join(convert_directory, filename))
         nt.assert_array_equal(ref_image, convert_image)
+
+
