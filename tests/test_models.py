96715034a7042c7b366185541a255bf79d100126
Christian Leibig <christian.leibig@uni-tuebingen.de>
2016-05-06 16:54:42 +0200

    Test correct output of JFnet
    passes for the current JFnet that can be applied
    to larger images and produces the same output
    as the net with dense layers (original implementation),
    two commits before. To be more specific, it produces
    the same output for 512 images and untied_biases.
    Tied_biases seem to change the posteriors a bit.
    
     Changes to be committed:
    	modified:   tests/test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -34,9 +34,44 @@ def test_jeffrey_df():
         assert l.output_shape == l_ref.output_shape
 
 
+def test_output_jeffrey_df():
+    import numpy as np
+    import theano
+    import theano.tensor as T
+    import lasagne
 
+    from datasets import KaggleDR
+    import models
 
+    weights = 'models/jeffrey_df/2015_07_17_123003_PARAMSDUMP.pkl'
+    network = models.jeffrey_df(width=512, height=512, filename=weights,
+                                untie_biases=True)
 
+    expected = np.array([[9.38881755e-01, 5.23291342e-02, 8.59508850e-03,
+                          1.34651185e-04, 5.94010562e-05],
+                         [9.19074774e-01, 6.69652745e-02, 1.35666728e-02,
+                          2.82015972e-04, 1.11185553e-04]], dtype=np.float32)
 
+    X = T.tensor4('inputs')
+    network['0'].input_var = X
+    img_dim = T.tensor4('img_dim')
+    network['22'].input_var = img_dim
+    prob = lasagne.layers.get_output(network['31'], deterministic=True)
 
+    forward_pass = theano.function([X, img_dim], prob)
 
+    kdr = KaggleDR(path_data='tests/ref_data/KDR/sample_JF_512',
+                   filename_targets='tests/ref_data/KDR/sampleLabels.csv',
+                   preprocessing=KaggleDR.jf_trafo)
+
+    inputs, targets = next(kdr.iterate_minibatches(np.arange(kdr.n_samples),
+                                                   batch_size=2, shuffle=False))
+    n_s = len(targets)
+    _img_dim = np.concatenate(
+        (np.full((n_s, 1, 1, 1), inputs.shape[2], dtype=np.float32),
+         np.full((n_s, 1, 1, 1), inputs.shape[3], dtype=np.float32)),
+        axis=1)/700.  # jeffrey does that under load_image_and_process?!
+    output =  forward_pass(inputs, _img_dim)
+    output = np.reshape(output, (2, 5))
+
+    nt.assert_array_almost_equal(expected, output)
