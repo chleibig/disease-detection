87f02e1fbc91f070a707a30f1ab07064f7613ddf
Christian Leibig <christian.leibig@uni-tuebingen.de>
2016-09-13 10:21:54 +0200

    Fix usage of scripts/convert_JF.py

--- scripts/prepare_tiny_imagenet.sh
+++ scripts/prepare_tiny_imagenet.sh
@@ -13,4 +13,4 @@ for image in $(find $path -name *.JPEG)
 done
 
 rm -rf $path/tiny-imagenet-200
-python scripts/convert_JF.py --directory=$path/raw --convert_directory=$path/JF_BG_512 --crop_size=512 --n_proc=$(nproc) --enhance_contrast=True --ignore_grayscale=True
+python scripts/convert_JF.py --directory=$path/raw --convert_directory=$path/JF_BG_512 --crop_size=512 --n_proc=$(nproc) --enhance_contrast --ignore_grayscale
